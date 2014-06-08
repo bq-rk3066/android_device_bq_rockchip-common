ec132e8c6b991af62bbdf9b85ba95e545defd335
Kra1o5 <kra1o5x@gmail.com>
2014-06-08 05:46:25 +0200

    Add cm.updater.uri for OTA updates

--- device-common.mk
+++ device-common.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
 
 # Default props
 PRODUCT_PROPERTY_OVERRIDES += \
+    cm.updater.uri=http://bokerones.noip.me/CyanogenModOTA/api \
     ro.opengles.version=131072 \
     ro.audio.flinger_standbytime_ms=1000 \
     ro.sf.lcdc_composer=0 \
